044aad0f55a2591cb9c1bf6fee8895f47d91638d
Rahuljain06 <72794264+Rahuljain06@users.noreply.github.com>
2023-01-18 13:53:22 +0530

    Time: 501 ms (94.43%), Space: 19.2 MB (22.88%) - LeetHub

---
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.py
@@ -0,0 +1,48 @@
+class Solution:
+    def maxSubarraySumCircular(self, nums: List[int]) -> int:
+        
+        #complexity O(n)
+        total=0
+        maxsum=float("-inf")
+        cur_max=0
+        minsum=float("inf")
+        cur_min=0
+
+        for i in range(len(nums)):
+            total+=nums[i]
+            
+            #kadane algorithm to find max sum subarray
+            cur_max+=nums[i]
+            maxsum=max(cur_max,maxsum)
+            if cur_max<0:
+                cur_max=0
+                
+            # finding the min subset and subarray it from total to get max sum subarray  
+            cur_min+=nums[i]
+            minsum=min(minsum,cur_min)
+            if cur_min>0:
+                cur_min=0
+        # if all the values are negative than we have the maximum of negatives
+        if total==minsum:
+            return maxsum
+        return max(maxsum,total-minsum)
+            
+        
+        
+        
+# This method has complexity O(n**2)
+#         maxi=float("-inf")
+#         def helper(nums):
+#             curr=0
+#             for i in range(len(nums)):
+#                 curr+=nums[i]
+#                 nonlocal maxi
+#                 maxi=max(curr,maxi)
+#                 if curr<0:
+#                     curr=0
+#             return maxi
+        
+#         for i in range(len(nums)):
+#             helper(nums[i:]+nums[:i])
+#         return maxi
+            
