33959c6c698faa3533f79e395043f887e484a744
Rahuljain06 <72794264+Rahuljain06@users.noreply.github.com>
2023-01-18 22:46:52 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (44.72%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -1,3 +1,9 @@
-class Solution:
-    def runningSum(self, nums: List[int]) -> List[int]:
-        return accumulate(nums)
+class Solution {
+    public int[] runningSum(int[] nums) {
+        
+        for (int i=1;i<nums.length;i++){
+            nums[i]+=nums[i-1];
+        }
+        return nums;      
+    }
+}
