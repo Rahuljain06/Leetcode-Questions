839704dc9a7fb1a1537366dc6c08c4ebe470b8f1
Rahuljain06 <72794264+Rahuljain06@users.noreply.github.com>
2023-01-21 20:10:27 +0530

    Time: 76 ms (20.19%), Space: 13.8 MB (79.81%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.py
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -1,19 +1,39 @@
 class Solution(object):
-    def restoreIpAddresses(self, s: str) -> List[str]:
-        rst = []
-        self.backtrack(s, 0, '', rst)
-        return rst
-    def backtrack(self,s, dots, path, rst):
-        if dots == 4:
-            #have 4 chunk and use up all digits
-            if not s: rst.append(path[:-1]) 
-            return
-        for i in range(1, 4):
-            #prevent index overflow
-            if i > len(s): continue
-            #take 1 digit is always good
-            #take 2 or 3 digits, first digit cannot be '0'
-            if i > 1 and s[0] == '0': continue
-            #take 3 digits, cannot greater than 255
-            if i > 2 and int(s[:3]) > 255: continue
-            self.backtrack(s[i:], dots+1, path + s[:i] + '.', rst)
+#    def restoreIpAddresses(self, s: str) -> List[str]:
+#         rst = []
+#         self.backtrack(s, 0, '', rst)
+#         return rst
+#     def backtrack(self,s, dots, path, rst):
+#         if dots == 4:
+#             #have 4 chunk and all the digits are used than we append
+#             if not s: rst.append(path[:-1]) # we are excluding the 4 and last dot here"255.255.11.135."
+#             return
+#         for i in range(1, 4):
+#             #prevent index overflow
+#             if i > len(s):
+#                 continue
+#             #take 1 digit is always good
+#             #take 2 or 3 digits, first digit cannot be '0'
+#             if i > 1 and s[0] == '0':
+#                 continue
+#             #take 3 digits, cannot greater than 255
+#             if i > 2 and int(s[:3]) > 255:
+#                 continue
+#             self.backtrack(s[i:], dots+1, path + s[:i] + '.', rst)
+            
+            
+            
+    def restoreIpAddresses(self, s):
+        res = []
+        self.dfs(s, 0, "", res)
+        return res
+    
+    def dfs(self, s, idx, path, res):
+        if idx > 4:
+            return 
+        if idx == 4 and not s:
+            res.append(path[:-1])
+            return 
+        for i in range(1, len(s)+1):
+            if s[:i]=='0' or (s[0]!='0' and 0 < int(s[:i]) < 256):
+                self.dfs(s[i:], idx+1, path+s[:i]+".", res)      
