674f65dc63f84973b2aa99bc30053e2e4f0fbf17
Rahuljain06 <72794264+Rahuljain06@users.noreply.github.com>
2023-02-01 22:45:24 +0530

    Time: 105 ms (99.54%), Space: 14.5 MB (38.19%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,5 +1,13 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        return [x[0] for x in sorted(zip(names,heights), key= lambda x: -x[1])]
+        #return [x[0] for x in sorted(zip(names,heights), key= lambda z: -z[1])]
         
+        d={}
+        for i,j in zip(names,heights):
+            d[j]=i
+        a=sorted(heights,key=lambda x:-x)
+        c=[]
+        for i in a:
+            c.append(d[i])
+        return c
        
