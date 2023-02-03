ba8e1d9fd8aacf8349a2754d1caa6abde10ea39c
Rahuljain06 <72794264+Rahuljain06@users.noreply.github.com>
2023-02-03 22:45:25 +0530

    Time: 52 ms (90.52%), Space: 14 MB (72.54%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -3,15 +3,13 @@ class Solution:
         if len(s)<numRows or numRows==1:
             return s
         a= [""]*numRows
-        
         par=0
         d=1
-        
         for i in s:
             a[par]+=i
             if par==0:
                 d=1
-            if par==numRows-1:
+            elif par==numRows-1:
                 d=-1
             par+=1*d
         return "".join(a)
