cff3e45b34374790711fceb73a3cf9bb429cb354
Rahuljain06 <72794264+Rahuljain06@users.noreply.github.com>
2023-02-04 22:16:51 +0530

    Time: 75 ms (75.27%), Space: 13.9 MB (64.59%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -3,20 +3,19 @@ class Solution:
         if len(s1)>len(s2):
             return False
         a=[0]*26
-        
         for i in range(len(s1)):
-            
             a[ord(s1[i])-ord("a")]+=1
             a[ord(s2[i])-ord("a")]-=1
         if a==[0]*26:
             return True
-        
+        print(a)
         for i in range(len(s1),len(s2)):
             a[ord(s2[i-len(s1)])-ord("a")]+=1
             a[ord(s2[i])-ord("a")]-=1
             
             if a==[0]*26:
                 return True
+            
         else:
             return False
         
