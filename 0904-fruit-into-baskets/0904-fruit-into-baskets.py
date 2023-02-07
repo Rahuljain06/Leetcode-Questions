dcdae84fe593f5fedee7920918b970ff8c6a88c5
Rahul Jain <72794264+Rahuljain06@users.noreply.github.com>
2023-02-07 23:03:06 +0530

    Time: 1013 ms (59.86%), Space: 20.1 MB (80.52%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -3,28 +3,15 @@ class Solution:
         l=0
         res=0
         d={}
-        for i,r in enumerate(fruits):
-            if r in d:
-                d[r]+=1
+        for r,i in enumerate(fruits):
+            if i in d:
+                d[i]+=1
             else:
-                d[r]=1
+                d[i]=1
             while len(d)>2:
                 d[fruits[l]]-=1
                 if not d[fruits[l]]:
                     d.pop(fruits[l])
                 l+=1
-            res=max(res,i-l+1)
+            res=max(res,r-l+1)
         return res
-    
-    
-    
-      # l, nums, res = 0, collections.Counter(), 0
-      #   for r in range(len(tree)):
-      #       nums[tree[r]] += 1
-      #       while len(nums) > 2:
-      #           nums[tree[l]] -= 1 
-      #           if not nums[tree[l]]:
-      #               nums.pop(tree[l])
-      #           l += 1
-      #       res = max(res, r - l + 1)
-      #   return res
