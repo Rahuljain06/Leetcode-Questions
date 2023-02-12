e52995d230dcaf2c76be595c957bf9ad1632cddf
Rahul Jain <72794264+Rahuljain06@users.noreply.github.com>
2023-02-12 22:56:30 +0530

    Time: 126 ms (78.39%), Space: 14.2 MB (11.89%) - LeetHub

--- 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.py
+++ 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.py
@@ -3,7 +3,7 @@ class Solution:
         if nums[0]>0 or nums[-1]<0 :
             return len(nums)
         s=0
-        e=len(nums)-1
+        e=len(nums)
         neg=0
         pos=0
         while s<e:
