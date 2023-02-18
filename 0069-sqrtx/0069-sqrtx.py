b2dcb016aa1dd8a2045756f37cb3f64a1900ea6a
Rahul Jain <72794264+Rahuljain06@users.noreply.github.com>
2023-02-18 22:51:15 +0530

    Time: 46 ms (46.66%), Space: 13.8 MB (47.95%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -3,10 +3,7 @@ class Solution:
         start=0
         end=x
         while start<=end:
-            
             mid=start+(end-start)//2
-            
-            
             if mid*mid<=x<(mid+1)*(mid+1):
                 return mid
             elif mid*mid>x:
