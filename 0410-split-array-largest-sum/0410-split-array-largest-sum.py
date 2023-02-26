4961309a747fe313afd85aa811e69096c6922a08
Rahul Jain <72794264+Rahuljain06@users.noreply.github.com>
2023-02-26 14:34:48 +0530

    Time: 43 ms (59.30%), Space: 13.9 MB (97.14%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.py
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.py
@@ -1,16 +1,22 @@
 class Solution:
-    def splitArray(self, nums: List[int], m: int) -> int:
-        lo, hi = max(nums), sum(nums)
-        while lo < hi:
-            mid = (lo+hi)//2
-            tot, cnt = 0, 1
-            for num in nums:
-                if tot+num<=mid: 
-                    tot += num
-                else:
-                    tot = num
-                    cnt += 1
-            if cnt>m: lo = mid+1
-            else: hi = mid
-        return hi
-        
+    def splitArray(self, weights: List[int], D: int) -> int:
+        def feasible(capacity) -> bool:
+            days = 1
+            total = 0
+            for weight in weights:
+                total += weight
+                if total > capacity:  # too heavy, wait for the next day
+                    total = weight
+                    days += 1
+                    if days > D:  # cannot ship within D days
+                        return False
+            return True
+
+        left, right = max(weights), sum(weights)
+        while left < right:
+            mid = left + (right - left) // 2
+            if feasible(mid):
+                right = mid
+            else:
+                left = mid + 1
+        return left
