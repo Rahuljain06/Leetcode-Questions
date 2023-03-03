e245491572ef125f4d66cea3e9cfb62ca1765dd8
Rahul Jain <72794264+Rahuljain06@users.noreply.github.com>
2023-03-03 22:37:12 +0530

    Time: 36 ms (41.63%), Space: 13.9 MB (11.03%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,9 +1,19 @@
 class Solution:
     def strStr(self, s: str, n: str) -> int:
-        for i in range(len(s) - len(n)+1):
-            if s[i:i+len(n)] == n:
-                return i
+        # for i in range(len(s) - len(n)+1):
+        #     if s[i:i+len(n)] == n:
+        #         return i
+        # return -1
+    
+        
+        
+        l=0
+        for r in range(len(n)-1,len(s)):
+            if s[l:r+1]==n:
+                return l
+            l+=1
         return -1
+                
 
 
 
