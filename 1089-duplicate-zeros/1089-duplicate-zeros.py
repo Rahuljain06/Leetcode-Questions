18441ef25f0272090c40adb89d08131aad1e1f25
Rahul Jain <72794264+Rahuljain06@users.noreply.github.com>
2023-03-19 15:02:57 +0530

    Time: 73 ms (60.02%), Space: 15 MB (19.41%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.py
+++ 1089-duplicate-zeros/1089-duplicate-zeros.py
@@ -5,16 +5,15 @@ class Solution:
         """
         n = len(arr)
         cntZero = arr.count(0)
-        newLen = n + cntZero # Length of new array if we don't trim up to length `n`
+        newLen = n + cntZero 
         
-        # Let's copy values from the end
         i = n - 1
         j = newLen - 1
         while i!=j:
             if j < n:
                 arr[j] = arr[i]
             j-=1
-            if arr[i] == 0: # Copy twice if arr[i] == 0
+            if arr[i] == 0:
                 if j < n: arr[j] = arr[i]
                 j -= 1
-            i-= 1
+            i-=1
