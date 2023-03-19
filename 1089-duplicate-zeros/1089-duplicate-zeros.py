26ed8b0dee65875f722fe44391db8ab51d357fa6
Rahul Jain <72794264+Rahuljain06@users.noreply.github.com>
2023-03-19 15:06:44 +0530

    Time: 66 ms (89.63%), Space: 14.9 MB (59.60%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.py
+++ 1089-duplicate-zeros/1089-duplicate-zeros.py
@@ -1,19 +1,19 @@
 class Solution:
-    def duplicateZeros(self, arr: List[int]) -> None:
+    def duplicateZeros(self, nums: List[int]) -> None:
         """
         Do not return anything, modify arr in-place instead.
         """
-        n = len(arr)
-        zeros = arr.count(0)
-        nlen = n + zeros
+
+        zeros = nums.count(0)
+        nlen = len(nums) + zeros
         
-        i = n - 1
+        i = len(nums) - 1
         j = nlen - 1
-        while i!=j:
-            if j < n:
-                arr[j] = arr[i]
+        while i<j:
+            if j < len(nums):
+                nums[j] = nums[i]
             j-=1
-            if arr[i] == 0:
-                if j < n: arr[j] = arr[i]
+            if nums[i] == 0:
+                if j < len(nums): nums[j] = nums[i]
                 j -= 1
             i-=1
