de3b1c632c2928bfb7480be319df015a2e7631a8
Rahul Jain <72794264+Rahuljain06@users.noreply.github.com>
2023-03-21 22:41:34 +0530

    Time: 64 ms (24.28%), Space: 13.9 MB (21.92%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -3,8 +3,10 @@ class Solution:
         if len(nums)<3:
             return len(nums)
         j=2
-        for i in range(2, len(nums)):
+        i=2
+        while(i<len(nums)):
             if nums[i]!=nums[j-2]:
                 nums[j] = nums[i]
                 j+=1
+            i+=1
         return j
