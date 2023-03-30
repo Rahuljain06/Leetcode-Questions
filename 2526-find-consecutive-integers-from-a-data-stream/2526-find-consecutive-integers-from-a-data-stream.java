3baab6d3910e1cff2d5b94a3570047f23c0b2e9c
Rahul Jain <72794264+Rahuljain06@users.noreply.github.com>
2023-03-31 00:05:58 +0530

    Time: 32 ms (77.17%), Space: 80 MB (37.50%) - LeetHub

---
+++ 2526-find-consecutive-integers-from-a-data-stream/2526-find-consecutive-integers-from-a-data-stream.java
@@ -0,0 +1,24 @@
+class DataStream {
+    int cnt=0,val,k;
+    public DataStream(int value, int k) {
+        this.val=value;
+        this.k=k;
+    }
+    
+    public boolean consec(int num) {
+        if (num==this.val){
+            cnt++;
+        }
+        else{
+            cnt=0;
+        }
+        return cnt>=this.k;
+        
+    }
+}
+
+/**
+ * Your DataStream object will be instantiated and called as such:
+ * DataStream obj = new DataStream(value, k);
+ * boolean param_1 = obj.consec(num);
+ */
