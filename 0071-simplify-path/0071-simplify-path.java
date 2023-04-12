0d51c116fdecd5249445dbb82ae91bd45a3e5661
Rahul Jain <72794264+Rahuljain06@users.noreply.github.com>
2023-04-12 23:40:42 +0530

    Time: 5 ms (52.40%), Space: 43 MB (21.18%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.java
+++ 0071-simplify-path/0071-simplify-path.java
@@ -1,7 +1,6 @@
 class Solution {
     public String simplifyPath(String path) {
         Stack<String> stack = new Stack<String>();
-        Set<String> skip = new HashSet<>(Arrays.asList("..", ".",""));
         for (String c : path.split("/")) {
             if (!stack.empty() && c.equals("..")) {
                 stack.pop();
