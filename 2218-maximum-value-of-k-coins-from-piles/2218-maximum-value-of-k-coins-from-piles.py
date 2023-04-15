51a20556bf4a12506e112acbf1e1ef28313ad036
Rahul Jain <72794264+Rahuljain06@users.noreply.github.com>
2023-04-16 00:02:43 +0530

    Time: 4448 ms (79.87%), Space: 140.9 MB (18.24%) - LeetHub

--- 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.py
+++ 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.py
@@ -1,11 +1,32 @@
 class Solution:
     def maxValueOfCoins(self, piles, k):
-        dp = [[0] * (k + 1) for _ in range(len(piles) + 1)]
-        for i in range(1, len(piles) + 1):
-            for j in range(1, k + 1):
-                cur = 0
-                for x in range(min(len(piles[i - 1]), j)):
-                    cur += piles[i - 1][x]
-                    dp[i][j] = max(dp[i][j], cur + dp[i - 1][j - x - 1])
-                dp[i][j] = max(dp[i][j], dp[i - 1][j])
-        return dp[len(piles)][k]
+        @functools.lru_cache(None)
+        def _search(rest_k: int, pile_pos: int) -> int:
+            """Searches for best accumulated coin value starting from pile at position `pile_pos`.
+            
+            Args:
+              - rest_k: The rest of coins we could pick.
+              - pile_pos: The starting position of pile we are working on.
+            """
+            if rest_k == 0 or pile_pos == len(piles): return 0
+
+            # Ignore the current pile and moving to next pile
+            current_mv_found = _search(rest_k, pile_pos+1)
+
+            # Start looking solution with current pile being involved
+            current_pile = piles[pile_pos]
+            accum_coin_value = 0
+            for i in range(min(rest_k, len(current_pile))):
+                # Accumulate the coins
+                accum_coin_value += current_pile[i]
+
+                # Moving to next pile with accumulated coin value in current pile
+                current_mv_found = max(
+                    current_mv_found, 
+                    accum_coin_value + _search(rest_k-i-1, pile_pos+1))
+
+            # Return the maximum accumulated coin value found from current pile.
+            return current_mv_found
+
+        # Start searching with initial `k` and first pile
+        return _search(k, 0)
