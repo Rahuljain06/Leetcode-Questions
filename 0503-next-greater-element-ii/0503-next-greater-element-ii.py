03a53a46b8b77c5d6e3fe68d672612509eca1d24
Rahul Jain <72794264+Rahuljain06@users.noreply.github.com>
2023-04-19 22:34:51 +0530

    Time: 198 ms (93.20%), Space: 15.6 MB (77.13%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.py
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.py
@@ -9,7 +9,7 @@ class Solution:
         for i in range(len(nums)):
             while stack and (nums[stack[-1]] < nums[i]):
                 res[stack.pop()] = nums[i]
-            stack.append(i)
+            
             
         return res
         
