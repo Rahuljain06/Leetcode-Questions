368cd0f15d35fad93715f9525591286135023826
Rahul Jain <72794264+Rahuljain06@users.noreply.github.com>
2023-04-19 22:34:45 +0530

    Time: 212 ms (72.64%), Space: 15.6 MB (77.13%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.py
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.py
@@ -1,18 +1,49 @@
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
-        nums2 = nums * 2
-        d={1:[]}
-        st=[]
-        res=[]
-        for x in nums2:
-            while st and st[-1]<x:
-                a=st.pop()
-                d[a]=d.get(a,[])+[x]
-            st.append(x)
-        for i in nums:
-            if i in d and len(d[i])>0:
-                res.append(d[i][0])
-                d[i].pop(0)
-            else:
-                res.append(-1)
+        stack=[]
+        res = [-1] * len(nums)
+        for i in range(len(nums)):
+            while stack and (nums[stack[-1]] < nums[i]):
+                res[stack.pop()] = nums[i]
+            stack.append(i)
+        for i in range(len(nums)):
+            while stack and (nums[stack[-1]] < nums[i]):
+                res[stack.pop()] = nums[i]
+            stack.append(i)
+            
         return res
+        
+        
+        
+        # nums2 = nums * 2
+        # d={}
+        # st=[]
+        # res=[]
+        # for x in nums2:
+        #     while st and st[-1]<x:
+        #         a=st.pop()
+        #         d[a]=d.get(a,[])+[x]
+        #     st.append(x)
+        # for i in nums:
+        #     if i in d and len(d[i])>0:
+        #         res.append(d[i][0])
+        #         d[i].pop(0)
+        #     else:
+        #         res.append(-1)
+        # return res
+        
+        
+#         stack = []
+#         res=[-1]*len(nums)
+#         for i in range(len(nums)) * 2:
+#             while stack and nums[stack[-1]] < nums[i]:
+#                 res[stack.pop()] -= nums[i]
+#             stack.append(i)
+#         return res
+    
+        # stack, res = [], [-1] * len(A)
+        #     for i in range(len(A)) * 2:
+        #         while stack and (A[stack[-1]] < A[i]):
+        #             res[stack.pop()] = A[i]
+        #         stack.append(i)
+        # return res
