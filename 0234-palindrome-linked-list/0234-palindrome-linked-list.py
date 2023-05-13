901580ad750c5e73ad25a210c664b6ff431fa5d2
Rahul Jain <72794264+Rahuljain06@users.noreply.github.com>
2023-05-13 23:38:09 +0530

    Time: 765 ms (71.43%), Space: 41.3 MB (66.18%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -0,0 +1,39 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        # a=[]  time complexity is O(n) but space complexity is O(n)
+        # while head:
+        #     a.append(head.val)
+        #     head=head.next
+        # if a==a[::-1]:
+        #     return True
+        # return False
+        
+        #Space Complexity O(1) and time complexity O(n)
+        slow=head
+        fast=head
+        while fast and fast.next:
+            slow=slow.next
+            fast=fast.next.next
+            
+        prev=None
+        nxt=None
+        while slow:
+            nxt=slow.next
+            slow.next=prev
+            prev=slow
+            slow=nxt
+            
+            
+        left,right=head,prev
+        while right:
+            if left.val!=right.val:
+                return False
+            left=left.next
+            right=right.next
+        return True
+        
