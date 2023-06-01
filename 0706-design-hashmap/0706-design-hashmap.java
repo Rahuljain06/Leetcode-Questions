dafead7e07d7437545a8e70bc592900356990796
Rahul Jain <72794264+Rahuljain06@users.noreply.github.com>
2023-06-02 00:51:39 +0530

    Time: 21 ms (75.16%), Space: 46.8 MB (91.42%) - LeetHub

---
+++ 0706-design-hashmap/0706-design-hashmap.java
@@ -0,0 +1,72 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class ListNode {
+    int[] pair;
+    ListNode next;
+    public ListNode(int key, int val) {
+        pair = new int []{key, val};
+        next=null;
+    }
+}
+
+class MyHashMap {
+    int m;
+    List<ListNode> h;
+    public MyHashMap() {
+        m = 1000;
+        h = new ArrayList<>();
+        for (int i = 0; i < m; i++) {
+            h.add(null);
+        }
+    }
+    public void put(int key, int value) {
+        int index = key % m;
+        if (h.get(index) == null) {
+            h.set(index, new ListNode(key, value));
+        } else {
+            ListNode cur = h.get(index);
+            while (true) {
+                if (cur.pair[0] == key) {
+                    cur.pair[1] = value;
+                    return;
+                }
+                if (cur.next == null) break;
+                cur = cur.next;
+            }
+            cur.next = new ListNode(key, value);
+        }
+    }
+    public int get(int key) {
+        int index = key % m;
+        ListNode cur = h.get(index);
+        while (cur != null) {
+            if (cur.pair[0] == key) {
+                return cur.pair[1];
+            } else {
+                cur = cur.next;
+            }
+        }
+        return -1;
+    }
+    public void remove(int key) {
+        int index = key % m;
+        ListNode cur = h.get(index);
+        if (cur == null) return;
+        if (cur.pair[0] == key) {
+            h.set(index, cur.next);
+        } else {
+            ListNode prev = cur;
+            cur = cur.next;
+            while (cur != null) {
+                if (cur.pair[0] == key) {
+                    prev.next = cur.next;
+                    break;
+                } else {
+                    prev = cur;
+                    cur = cur.next;
+                }
+            }
+        }
+    }
+}
