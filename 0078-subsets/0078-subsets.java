f48f781e3bfa69894d99207d40e70c34f028c418
Rahul Jain <72794264+Rahuljain06@users.noreply.github.com>
2023-06-24 23:39:57 +0530

    Time: 1 ms (73.68%), Space: 42 MB (79.76%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,7 +1,3 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
@@ -10,11 +6,11 @@ class Solution {
     }
 
     private void dfs(int[] nums, List<Integer> path, List<List<Integer>> res) {
-        res.add(new ArrayList<>(path)); // Make a copy of the path and add it to res
+        res.add(path);
         for (int i = 0; i < nums.length; i++) {
             List<Integer> c=new ArrayList<>(path);
             c.add(nums[i]);
-            dfs(Arrays.copyOfRange(nums, i + 1, nums.length),c, res); // Pass a copy of the path // Modify the original path list
+            dfs(Arrays.copyOfRange(nums, i + 1, nums.length),c,res);
         }
     }
 }
