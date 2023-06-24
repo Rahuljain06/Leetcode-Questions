0c4c213a61dd58ec6f8904d1e26068bc88c50ebb
Rahul Jain <72794264+Rahuljain06@users.noreply.github.com>
2023-06-25 01:28:10 +0530

    Time: 1 ms (73.62%), Space: 41.9 MB (86.82%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,17 +1,16 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-    List<List<Integer>> list = new ArrayList<>();
-    Arrays.sort(nums);
-    backtrack(list, new ArrayList<>(), nums, 0);
-    return list;
-}
-
-private void backtrack(List<List<Integer>> list , List<Integer> tempList, int [] nums, int start){
-    list.add(new ArrayList<>(tempList));
-    for(int i = start; i < nums.length; i++){
-        tempList.add(nums[i]);
-        backtrack(list, tempList, nums, i + 1);
-        tempList.remove(tempList.size() - 1);
+        List<List<Integer>> res = new ArrayList<>();
+        dfs(nums, new ArrayList<>(), res);
+        return res;
     }
-}
+
+    private void dfs(int[] nums, List<Integer> path, List<List<Integer>> res) {
+        res.add(path);
+        for (int i = 0; i < nums.length; i++) {
+            List<Integer> c=new ArrayList<>(path);
+            c.add(nums[i]);
+            dfs(Arrays.copyOfRange(nums, i + 1, nums.length),c,res);
+        }
+    }
 }
