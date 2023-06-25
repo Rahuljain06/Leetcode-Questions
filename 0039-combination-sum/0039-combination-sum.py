28ce8c0b72c4322fa60eebce9c56916e8f5f3e9b
Rahul Jain <72794264+Rahuljain06@users.noreply.github.com>
2023-06-26 00:32:33 +0530

    Time: 79 ms (54.23%), Space: 16.4 MB (84.03%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -6,11 +6,11 @@ class Solution:
     
     def dfs(self,nums,target,path,res):
         if target<0:
-            return #backtracking
+            return #backtracking isme ham add kar rhe hain path main or jab sum bada ho jaa rha ta backtrack kar rhe hai
         if target==0:
             res.append(path)
             return
         for i in range(len(nums)):
-            if target<nums[i]:
-                continue
+            # if target<nums[i]: ham isme value add hi nahi kar rhe, path me jo target se badi ho
+            #     continue 
             self.dfs(nums[i:],target-nums[i],path+[nums[i]],res)
