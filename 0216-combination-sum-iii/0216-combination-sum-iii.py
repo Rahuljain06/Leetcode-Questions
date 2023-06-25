616aa03ed3fe7a9b46fbb0cd372c0c871ccaf21a
Rahul Jain <72794264+Rahuljain06@users.noreply.github.com>
2023-06-26 00:27:45 +0530

    Time: 45 ms (73.93%), Space: 16.4 MB (47.68%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.py
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -5,13 +5,15 @@ class Solution(object):
         self.dfs(range(1,10),[],target,k,res)
         return res
     
-    def dfs(self,nums,path,target,k,res):   
+    def dfs(self,nums,path,target,k,res):
+        if k<0 or target<0: 
+            return #backtracking isme ham add kar rhe hain path main or jab sum bada ho jaa rha ta backtrack kar rhe hai or k ki value 0 se choti ho rhi hai to matlab elements path mai k se jyada ho rhe hai to backtrack karenge       
         if target==0 and k==0:
             res.append(path)
             return
         for i in range(len(nums)):
-            if target<nums[i]:
-                continue
+            # if target<nums[i]:#ham isme value add hi nahi kar rhe, path me jo target se badi ho
+                # continue
             self.dfs(nums[i+1:],path+[nums[i]],target-nums[i],k-1,res)
 
         
