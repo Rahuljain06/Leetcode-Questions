94d084dc5c60201e1e7a884c3a0715b65b5612a2
Rahul Jain <72794264+Rahuljain06@users.noreply.github.com>
2023-06-26 00:10:42 +0530

    Time: 35 ms (97.88%), Space: 16.4 MB (12.26%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -0,0 +1,18 @@
+class Solution(object):
+
+    def combinationSum3(self, k, target):
+        res=[]
+        self.dfs(range(1,10),[],target,k,res)
+        return res
+    
+    def dfs(self,nums,path,target,k,res):
+        if k<0 or target<0:
+            return
+        if target==0 and k==0:
+            res.append(path)
+            return
+        for i in range(len(nums)):
+            self.dfs(nums[i+1:],path+[nums[i]],target-nums[i],k-1,res)
+
+        
+            
