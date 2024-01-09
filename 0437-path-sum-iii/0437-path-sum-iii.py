5cf9189cd84b9cc9626869a646944e5d238bd203
Rahul Jain <72794264+Rahuljain06@users.noreply.github.com>
2024-01-09 14:15:49 +0530

    Time: 356 ms (33.47%), Space: 17.7 MB (90.56%) - LeetHub

---
+++ 0437-path-sum-iii/0437-path-sum-iii.py
@@ -0,0 +1,40 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], target: int) -> int:
+        #Approach ham poore tree me traverse karenge or har node par dfs lagakar ye check karenge ki waha se koi bhi path aisa hai jiska sum target ke equal hai
+        # define global return var
+        self.numOfPaths = 0
+        # 1st layer DFS to go through each node
+        self.dfs(root, target)
+        # return result
+        return self.numOfPaths
+    
+    # define: traverse through the tree, at each treenode, call another DFS to test if a path sum include the answer
+    def dfs(self, node, target):
+        # exit condition
+        if node is None:
+            return 
+        # dfs break down 
+        self.test(node, target) # you can move the line to any order, here is pre-order
+        self.dfs(node.left, target)
+        self.dfs(node.right, target)
+        
+    # define: for a given node, DFS to find any path that sum == target, if find self.numOfPaths += 1
+    def test(self, node, target):
+        # exit condition
+        if node is None:
+            return
+        if node.val == target:
+            self.numOfPaths += 1
+            
+        # test break down
+        self.test(node.left, target-node.val)
+        self.test(node.right, target-node.val)
+        
+        
+        
