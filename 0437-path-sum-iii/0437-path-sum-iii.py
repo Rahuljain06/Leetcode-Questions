16e430bd73ca2d0ca769422b6cffa4dbda0dacd6
Rahul Jain <72794264+Rahuljain06@users.noreply.github.com>
2024-01-10 09:36:01 +0530

    Time: 347 ms (34.10%), Space: 22.3 MB (10.29%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.py
+++ 0437-path-sum-iii/0437-path-sum-iii.py
@@ -8,6 +8,8 @@ class Solution:
     # hamne count esiliye use kiya kyunki jab ham pathsum me iterate karenge to vo har baar 0 ho jayega to hame globally count chaiye tha to maine constructor par daal diya.
     def __init__(self):
         self.cnt=0
+        self.dp={}
+    @cache
     def pathSum(self, root: Optional[TreeNode], target: int) -> int:
         #cnt=0 # ye esiliye work nhi karega kyunki tab ham traverse kaege pathsum me o ye har baar 0 ho jayega or hame total nhi milega.
         #Approach: ham poore tree me traverse karenge or har node par dfs lagakar ye check karenge ki waha se koi bhi path aisa hai jiska sum target ke equal hai
