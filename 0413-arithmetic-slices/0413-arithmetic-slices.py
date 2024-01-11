77fb454be821a19fc76ca625d0918fd5ce5288e0
Rahul Jain <72794264+Rahuljain06@users.noreply.github.com>
2024-01-11 09:45:17 +0530

    Time: 39 ms (75.17%), Space: 17.5 MB (68.54%) - LeetHub

--- 0413-arithmetic-slices/0413-arithmetic-slices.py
+++ 0413-arithmetic-slices/0413-arithmetic-slices.py
@@ -1,5 +1,7 @@
 class Solution:
     def numberOfArithmeticSlices(self, nums: List[int]) -> int:
+        # APPROACH: Ham Yaha 2 se start kar rhe hai loop kyunki hame minimum 3 length ki subarray chaiye to ham 2 index peeche jaakr check kar sake ki diff of curr index - prev index and diff of prev index- prev2 index agar equal ata hai to uski lenght alteast 3 hogi or ham apne dp me append kar denhge. ab problem hai tab 4 ki hogi to hame 2 add karna hoga 1 ki jgha [look at the first example] to ham dp[i-1]+1 karenge kyunki aar peechle index par koi bhi subarrray bani hogi to wha 1 hoga already to hame usme 1 or add kar denge for eg 1234 to 123 ka 1 and 1234 ka 2 or last me ham dp ka sum return na karna pade esiliye pehle h count me store kar lenge jisse ek iteratio kam chale
+        
         dp=[0]*len(nums)
         cnt=0
         for i in range(2,len(nums)):
