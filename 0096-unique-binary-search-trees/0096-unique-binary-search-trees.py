6946a7eb8876a67d9c6316ccf40e85ca999de7a8
Rahul Jain <72794264+Rahuljain06@users.noreply.github.com>
2024-01-13 01:20:49 +0530

    Time: 35 ms (73.05%), Space: 17.4 MB (14.15%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
@@ -5,7 +5,8 @@ class Solution:
             return 1
         res=0
         for i in range(n):
+            #CATALAN NUMBER
             #C0C2+C1C1+C2C0 for N=3
-            #C0C3+C1C2+C2C1+C3C0 for  N=4
+            #C0C3+C1C2+C2C1+C3C0 for N=4
             res+=self.numTrees(i)*self.numTrees(n-i-1)
         return res
