7f6c6482baf921f571fc37f9712e6a1b6d189aa4
Rahul Jain <72794264+Rahuljain06@users.noreply.github.com>
2024-01-13 00:31:56 +0530

    Time: 38 ms (54.97%), Space: 17.3 MB (25.56%) - LeetHub

---
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
@@ -0,0 +1,11 @@
+class Solution:
+    @cache
+    def numTrees(self, n: int) -> int:
+        if n<=1:
+            return 1
+        res=0
+        for i in range(n):
+            #C0C2+C1C1+C2C0 for N=3
+            #C0C3+C1C2+C2C1+C3C0 for  N=4
+            res+=self.numTrees(i)*self.numTrees(n-i-1)
+        return res
