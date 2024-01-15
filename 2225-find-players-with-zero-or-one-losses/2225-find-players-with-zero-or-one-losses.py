2d5eaa4fd9883833a43cafbbfb91bf31e45604bb
Rahul Jain <72794264+Rahuljain06@users.noreply.github.com>
2024-01-15 23:51:10 +0530

    Time: 1813 ms (13.23%), Space: 72.8 MB (13.18%) - LeetHub

---
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
@@ -0,0 +1,24 @@
+from sortedcontainers import SortedDict
+class Solution:
+    def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
+        ans=[[],[]]
+        d=SortedDict()
+        for i,j in matches:
+            if i not in d:
+                d[i]=1
+            if j in d:
+                d[j]-=1
+            else:
+                d[j]=0
+        for i in d:
+            if d[i]==1:
+                ans[0].append(i)
+            if d[i]==0:
+                ans[1].append(i)
+        return ans
+        
+            
+                
+                
+        
+        
