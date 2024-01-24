9c668dccae334ca5dee90a9ac1546a6f839eb88f
Rahul Jain <72794264+Rahuljain06@users.noreply.github.com>
2024-01-24 12:53:18 +0530

    Time: 410 ms (89.33%), Space: 43.5 MB (98.67%) - LeetHub

---
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pseudoPalindromicPaths (self, root: Optional[TreeNode]) -> int:
+        cnt=0
+        def palindrome(d):
+            nonlocal cnt
+            miss=False
+            for i in d:
+                if d[i]%2!=0:
+                    if miss:
+                        return False
+                    miss=True
+            cnt+=1
+        def dfs(root,res):
+            nonlocal cnt
+            if not root:
+                return None
+            res[root.val]= res[root.val]+1
+            if not root.left and not root.right:
+                if palindrome(res):
+                    cnt+=1
+            dfs(root.left,res)
+            dfs(root.right,res)
+            res[root.val]= res[root.val]-1
+        dfs(root,defaultdict(int))
+        
+        return cnt
+        
+            
+        
