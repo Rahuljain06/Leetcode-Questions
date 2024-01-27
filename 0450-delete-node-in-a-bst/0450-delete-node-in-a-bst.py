245ee2c8573e2b65dfede509530c2b680c81633b
Rahul Jain <72794264+Rahuljain06@users.noreply.github.com>
2024-01-27 17:42:36 +0530

    Time: 64 ms (49.75%), Space: 19.9 MB (96.68%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
@@ -13,11 +13,15 @@ class Solution:
         elif root.val<key:
             root.right=self.deleteNode(root.right,key)
         else:
+            #pehle hamne check kiya ki root.left hai ya nhi agar nhi ho to directly root.right return kar diya [Trick: isme ye ho sakta hai root.right bhi na ho to None return ho jayega usse hame fark nhi padta]
             if not root.left:
                 return root.right
+            #fir hamne check kiya ki root.right hai ya nhi agar nhi ho to directly root.left return kar diya
             elif not root.right:
                 return root.left
             else:
+                #agar dono exist left and right to BST ki property maintain karne ke liye hame root ke right node me jaakr minimum dhoondna hoga and uss node ki val ko apne key node wali value se replace karna hoga & fir baad me uss node ko hatana hoga
+                
                 minNode=self.findmin(root.right)
                 root.val=minNode.val
                 root.right=self.deleteNode(root.right,minNode.val)
