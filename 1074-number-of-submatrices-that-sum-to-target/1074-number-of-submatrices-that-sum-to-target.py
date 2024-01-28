f86e0133891c0d3f27ea834c69d18588b79fff30
Rahul Jain <72794264+Rahuljain06@users.noreply.github.com>
2024-01-28 11:54:21 +0530

    Time: 409 ms (87.35%), Space: 17.7 MB (63.67%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.py
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.py
@@ -7,21 +7,12 @@ class Solution:
                 matrix[x][y+1] += matrix[x][y]
         #print(matrix)
         res = 0
-        for y1 in range(n):
-            for y2 in range(y1, n):
-        # d={0:1}
-        # cursum=0
-        # count=0
-        # for i in nums:
-        #     cursum+=i
-        #     if cursum-k in d:
-        #         count+=d[cursum-k]
-        #     d[cursum]=d.get(cursum,0)+1
-        # return count
+        for c1 in range(n):
+            for c2 in range(c1, n):
                 d = {0: 1}
                 cursum = 0
-                for x in range(m):
-                    cursum += matrix[x][y2] - (matrix[x][y1-1] if y1 > 0 else 0)
+                for r in range(m):
+                    cursum += matrix[r][c2] - (matrix[r][c1-1] if c1 > 0 else 0)
                     if cursum-target in d:
                         res += d[cursum - target]
                     d[cursum] = d.get(cursum, 0) + 1
