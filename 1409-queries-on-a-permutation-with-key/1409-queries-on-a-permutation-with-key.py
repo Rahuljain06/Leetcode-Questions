c5dd884e2cc322436edd94596fae5b74448c2269
Rahul Jain <72794264+Rahuljain06@users.noreply.github.com>
2024-01-30 00:48:51 +0530

    Time: 60 ms (58.76%), Space: 16.7 MB (73.93%) - LeetHub

--- 1409-queries-on-a-permutation-with-key/1409-queries-on-a-permutation-with-key.py
+++ 1409-queries-on-a-permutation-with-key/1409-queries-on-a-permutation-with-key.py
@@ -2,7 +2,7 @@ class Solution:
     def processQueries(self, queries: List[int], m: int) -> List[int]:
         nums=[i+1 for i in range(m)]
         res=[]
-        for i,val in enumerate(queries):
+        for val in queries:
             res.append(nums.index(val))
             nums.remove(val)
             nums.insert(0,val)
