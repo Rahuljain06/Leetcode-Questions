024ee82c450a6998994a8e0a01526656d3b08d5e
Rahul Jain <72794264+Rahuljain06@users.noreply.github.com>
2024-02-05 07:18:26 +0530

    Time: 33 ms (97.67%), Space: 16.9 MB (58.40%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.py
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -1,6 +1,19 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         
+        # iss question me 3 cases hai:
+        # 1: jab p[j+1]=="*" ho 
+            #iss case ke liye 2 option use and not use
+                # use ham tabhi kar sakte jab 2 string match ho (s[i]==p[j]) to ham s wali string badayenge par j wale ko nhi kyunki ham uske * ko agli baar bhi use kar sakte hai.
+                # not use karenge to simply hame p[j] wali string ko +2 karna hai jisse current character and agla character * skip ho jaye.
+        
+        # 2: jab p[j]=="." ho
+            # jab p[j] par . hoga to vo koi bhi s[i] ki character se match hoga to hame usko alag se handle karne ki jaroorat nhi hai. vo simple match wale case me #3 case me aa jyega
+        # 3: jab s[i]==p[j] ho
+            # jab current index par 2 character match ho to simply i and j dono me aage bad jana hai 
+        # Important: hamne i>=len(s) par False esiliye nhi kiya kyunki ho sakta hai ki p me extra characters ho with * to jaroori nhi ki ham s khatam ho jaye to ham directly False return kar de. 
+        #Eg s="a" p="a*b*" to iska ans true ana chaiye kyunki ham isme b* ko ek baar bhi nhi lenge and a* ko ek baar lenge to esiliye tab ham poori string kar check kar lenge tab hi to return karenge.
+    
         dp={}
         def dfs(i,j):
             if i>=len(s) and j>=len(p):
