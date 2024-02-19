dd426678a718108afed720aa2acec4974a0e4f16
Rahul Jain <72794264+Rahuljain06@users.noreply.github.com>
2024-02-19 22:53:47 +0530

    Time: 1180 ms (74.67%), Space: 62.3 MB (82.95%) - LeetHub

--- 2402-meeting-rooms-iii/2402-meeting-rooms-iii.py
+++ 2402-meeting-rooms-iii/2402-meeting-rooms-iii.py
@@ -2,18 +2,18 @@ class Solution:
     def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
         free_rooms = list(range(n))
         heapify(free_rooms)
-        meetings_info = []
+        meetings_info = [] # [endtime,room]
         num_rooms = [0] * n
         for start, end in sorted(meetings):
             while meetings_info and meetings_info[0][0] <= start:
                 _, room_number = heappop(meetings_info)
                 heappush(free_rooms, room_number)
-            if free_rooms:
-                room_number = heappop(free_rooms)
-                heappush(meetings_info, (end, room_number))
-            else:
-                end_time, room_number = heappop(meetings_info)
-                heappush(meetings_info, (end_time + end - start, room_number))
-            num_rooms[room_number] += 1
+            if not free_rooms:
+                end_time, room = heappop(meetings_info)
+                end=end_time+end-start
+                heappush(free_rooms,room)
+            room=heappop(free_rooms)
+            heappush(meetings_info,(end,room))
+            num_rooms[room] += 1
 
         return num_rooms.index(max(num_rooms))
