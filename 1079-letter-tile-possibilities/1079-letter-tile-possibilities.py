2da77d11e7106f75c5894a24e35d8b500934cfc8
Rahul Jain <72794264+Rahuljain06@users.noreply.github.com>
2024-02-23 00:23:51 +0530

    Time: 86 ms (50.00%), Space: 18 MB (59.48%) - LeetHub

--- 1079-letter-tile-possibilities/1079-letter-tile-possibilities.py
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.py
@@ -3,9 +3,8 @@ class Solution:
         # 46 permutation jaisa hai 
         res=set()
         self.dfs(tiles, "",res)
-        return len(res)
+        return len(res)-1
     def dfs(self,nums,path,res):
-        if path: # ye hamne seif empty string add na ho esiliye lagayi hai
-            res.add(path)
+        res.add(path)
         for i in range(len(nums)):
             self.dfs(nums[:i]+nums[i+1:],path+nums[i],res)
