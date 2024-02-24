13eff40956500fba3e8240f636740a57eda6ee4f
Rahul Jain <72794264+Rahuljain06@users.noreply.github.com>
2024-02-24 23:26:40 +0530

    Time: 1644 ms (51.65%), Space: 71.2 MB (11.92%) - LeetHub

--- 2092-find-all-people-with-secret/2092-find-all-people-with-secret.py
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.py
@@ -1,5 +1,6 @@
 class Solution:
     def findAllPeople(self, n: int, meetings: List[List[int]], firstPerson: int) -> List[int]:
+        # basically ham pehle time ko key mankar ek dictionary banenge and uss ke andar jaakar har person ko map karenge uske baad ham sorted karke dictionary ke keys ko pata laga lenge ki iss time tak kitne logo ko secrets pata tha or kis kis ki meeting same time par ya uske baad hui hai or un logo ko secrets ke andar daalte jaayenge or last mai secrets return kar denge.
         secrets=set([0,firstPerson]) # person who knows secrets
         timemap={}
         
@@ -20,8 +21,7 @@ class Solution:
         for t in sorted(timemap.keys()):
             visited=set()
             for src in timemap[t]:
-                if src in secrets: #secret tabhi failega agar kisi bhi src ko pata hoga
+                if src in secrets: #secret tabhi failega agar kisi bhi person ko pata hoga uss prticular time par 
                     dfs(src, timemap[t])
             
-        
         return list(secrets)
