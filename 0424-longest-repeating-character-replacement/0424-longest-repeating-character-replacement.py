1f3515a4662241d18d753b3fc5d816e6bc3ea747
Rahul Jain <72794264+Rahuljain06@users.noreply.github.com>
2024-03-09 21:42:47 +0530

    Time: 88 ms (62.39%), Space: 16.8 MB (23.41%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,5 +1,7 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
+        
+        # iss question me ham basically ham r pointer bada rhe hai tab tak ki windowsize-frequency of max character <=k tak hai kyunki for eg: AABAA k=1 to isme mai longest substring 5 aayegi kyunki isme window size kitni hai= 5 or sabse jyada baar konsa character repeat hua A 4 baar to hame iss window sirf B ko hi to change karna hai or fir ans mil jayega to 5-4<=1 to condition thik hai to chal jayegi or res me ham 5 store kar lenge par agar condition met nhi hogi to ham left ko bada denge or uski character ki frequency ham hashmap se - kar denge or fir max check karenge r badakar ki uss window me condition met ho rhi hai kya and aise last tak check karenge....
         d={}
         l= 0
         res=0
