e811a2ec8e31f5746973fbe32569867de2a324ab
Rahul Jain <72794264+Rahuljain06@users.noreply.github.com>
2024-03-11 12:53:26 +0530

    Time: 98 ms (26.81%), Space: 16.7 MB (22.45%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.py
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -1,10 +1,13 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        res=0
-        idx=1
-        for i in range(len(num2)-1,-1,-1):
-            res+=(int(num1)*int(num2[i])*idx)
-            idx*=10
-        return str(res)
+        res = [0]* (len(num1) + len(num2))
+        for i, n1 in enumerate(reversed(num1)):
+            for j, n2 in enumerate(reversed(num2)):
+                res[i+j] += int(n1) * int(n2)
+                res[i+j+1] += res[i+j]//10
+                res[i+j] %= 10
+    
+        while len(res) > 1 and res[-1] == 0: res.pop()
+        return ''.join(map(str,res[::-1]))
             
             
