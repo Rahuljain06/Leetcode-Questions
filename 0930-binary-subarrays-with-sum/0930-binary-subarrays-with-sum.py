f65cd8a865485d1ce4e78e0b80c008b18789bfb5
Rahul Jain <72794264+Rahuljain06@users.noreply.github.com>
2024-03-14 23:31:00 +0530

    Time: 225 ms (45.29%), Space: 20.4 MB (45.37%) - LeetHub

---
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], k: int) -> int:
+        d={0:1}
+        cursum=0
+        count=0
+        for i in nums:
+            cursum+=i
+            if cursum-k in d:
+                count+=d[cursum-k]
+            d[cursum]=d.get(cursum,0)+1
+        return count
