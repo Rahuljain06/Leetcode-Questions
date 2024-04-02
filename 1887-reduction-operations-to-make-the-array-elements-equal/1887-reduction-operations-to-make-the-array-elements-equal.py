279d30eb754e194e17cc18f84737b607bf84a07a
Rahul Jain <72794264+Rahuljain06@users.noreply.github.com>
2024-04-02 12:44:07 +0530

    Time: 821 ms (16.90%), Space: 27.6 MB (37.46%) - LeetHub

--- 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.py
+++ 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.py
@@ -2,7 +2,7 @@ class Solution:
     def reductionOperations(self, nums: List[int]) -> int:
         c=Counter(nums)
         res=0
-        d=sorted(c,key=lambda x:(-x,c[x]))
+        d=sorted(c,key=lambda x:(-x))
         for i,val in enumerate(d):
             res+=c[val]*(len(d)-i-1)
         return res
