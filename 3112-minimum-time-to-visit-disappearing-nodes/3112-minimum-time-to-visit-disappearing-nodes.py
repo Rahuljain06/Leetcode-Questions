0866df8e84a2d8c886c21f3fa02f7f0b5a15d64c
Rahul Jain <72794264+Rahuljain06@users.noreply.github.com>
2024-04-19 17:33:11 +0530

    Time: 1502 ms (21.87%), Space: 65.1 MB (45.98%) - LeetHub

--- 3112-minimum-time-to-visit-disappearing-nodes/3112-minimum-time-to-visit-disappearing-nodes.py
+++ 3112-minimum-time-to-visit-disappearing-nodes/3112-minimum-time-to-visit-disappearing-nodes.py
@@ -1,22 +1,23 @@
 class Solution:
     def minimumTime(self, n: int, edges: List[List[int]], disappear: List[int]) -> List[int]:
-        
+        #simple dijastrik hai     
         graph=defaultdict(list)
         ans=[-1]*n
         
         for u,v,t in edges:
             graph[u].append((t,v))
             graph[v].append((t,u))
+            
         heap=[(0,0)]
         
         while heap:
-            time,node=heapq.heappop(heap)
+            spendedtime,node=heapq.heappop(heap)
             if ans[node]!=-1:continue
-            ans[node]=time
+            ans[node]=spendedtime
             
-            for time2, newnode in graph[node]:
-                time2+=time
-                if time2<disappear[newnode]:
-                    heapq.heappush(heap,(time2,newnode))
+            for newtime, newnode in graph[node]:
+                newtime+=spendedtime # isme hamne abhi tak spend kiya hua time + newtime ko add kar diya or agar vo disapper[node] ke time se kam hai tabhi ham uss node tak pahoch jayenge otherwise -1
+                if newtime<disappear[newnode]:
+                    heapq.heappush(heap,(newtime,newnode))
         return ans
             
