28e10b9a0f50c845b7de2c80059c0a6c1516ba30
Rahul Jain <72794264+Rahuljain06@users.noreply.github.com>
2024-04-20 16:41:59 +0530

    Time: 811 ms (49.61%), Space: 26.4 MB (5.82%) - LeetHub

--- 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
@@ -20,14 +20,3 @@ class Solution:
             if dfs(i):
                 res.append(i)
         return res
-    
-#     def dfs(r):
-#             if r not in can_make:
-#                 can_make[r] = False
-#                 if r in graph:
-#                     can_make[r] = all([dfs(i) for i in graph[r]])
-#             return can_make[r]
-        
-#         can_make = {s: True for s in supplies}
-#         graph = {r : ing for r, ing in zip(recipes, ingredients)}
-#         return [r for r in recipes if dfs(r)]
