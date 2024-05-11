1e60ed244e29f6a4085d1a7dc1ac83512763549e
Rahul Jain <72794264+Rahuljain06@users.noreply.github.com>
2024-05-12 00:32:28 +0530

    Time: 156 ms (54.60%), Space: 19 MB (52.82%) - LeetHub

--- 0857-minimum-cost-to-hire-k-workers/0857-minimum-cost-to-hire-k-workers.py
+++ 0857-minimum-cost-to-hire-k-workers/0857-minimum-cost-to-hire-k-workers.py
@@ -1,10 +1,18 @@
 class Solution:
     def mincostToHireWorkers(self,quality, wage, K):
-        workers = sorted((w/q, w, q) for w, q in zip(wage, quality))
-        cost, team, sumq = float('inf'), [], 0
+        # hame basically k workers equal pay karna hai and vo wage/quality se nikal lenge. ye bilkul waise hi ki per hr kitna pay karna hai.. to hame ye vlue minimize karni hai to uske liye ham pehle ratio nikalkar sort kar lenge jisse hame  fir usme se k peoples select karenge and fir 
+        workers=[]
+        for w, q in zip(wage, quality):
+            workers.append((w/q, w, q))
+        workers.sort()
+        cost= float('inf')
+        team=[]
+        totalq=0
         for ratio, w, q in workers:
             heapq.heappush(team, -q)
-            sumq += q
-            if len(team) > K: sumq += heapq.heappop(team)
-            if len(team) == K: cost = min(cost, sumq * ratio)   
+            totalq+= q
+            if len(team) > K:
+                totalq += heapq.heappop(team) # ham yaha par add esiliye kar rhe hai kyuni hamne values -ve me daali hai kyunki ye maxheap hai
+            if len(team) == K:
+                cost = min(cost, totalq * ratio)   
         return cost
