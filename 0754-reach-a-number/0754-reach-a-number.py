5b3377bda138b9317a929091714e3a492039c265
Rahul Jain <72794264+Rahuljain06@users.noreply.github.com>
2024-05-13 21:53:23 +0530

    Time: 79 ms (9.14%), Space: 16.6 MB (63.96%) - LeetHub

--- 0754-reach-a-number/0754-reach-a-number.py
+++ 0754-reach-a-number/0754-reach-a-number.py
@@ -21,6 +21,9 @@ class Solution:
         while (csum<target):
             steps+=1
             csum+=steps
+            if csum==target:
+                return steps
+        
         
         while (csum-target)%2!=0:
             steps+=1
