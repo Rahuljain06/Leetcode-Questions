0a572f1a92f4f89f5c90c3e5f66016b638a8e68f
Rahul Jain <72794264+Rahuljain06@users.noreply.github.com>
2024-05-28 11:53:59 +0530

    Time: 7996 ms (40.87%), Space: 43.6 MB (30.94%) - LeetHub

--- 3164-find-the-number-of-good-pairs-ii/3164-find-the-number-of-good-pairs-ii.py
+++ 3164-find-the-number-of-good-pairs-ii/3164-find-the-number-of-good-pairs-ii.py
@@ -1,20 +1,25 @@
 class Solution:
     def numberOfPairs(self, nums1: List[int], nums2: List[int], k: int) -> int:
-        n = len(nums1)
-        m = len(nums2)
-        cnt = 0
-        mapi = Counter(nums2)
-        for i in range(n):
-            if nums1[i] % k != 0:
-                continue
-            val = nums1[i] // k
-            for j in range(1, isqrt(val) + 1):
-                if val % j == 0:
-                    cnt += mapi[j]
-                    if j != val // j:
-                         cnt += mapi[val // j]
-        return cnt
+         # Create a hashmap to store counts of nums2[j] * k
+        divisor_count = defaultdict(int)
         
+        # Populate the hashmap with counts of nums2[j] * k
+        for num in nums2:
+            divisor_count[num * k] += 1
         
-
+        good_pairs = 0
+        
+        # For each number in nums1, find all its divisors and check against the hashmap
+        for num in nums1:
+            for j in range(1, int(num ** 0.5) + 1):
+                if num % j == 0:
+                    # j is a divisor
+                    if j in divisor_count:
+                        good_pairs += divisor_count[j]
+                    # num // j is also a divisor if it's different from j
+                    if j != num // j and num // j in divisor_count:
+                        good_pairs += divisor_count[num // j]
+        
+        return good_pairs
+       
                     
