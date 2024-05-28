cc5050d65d6f6106282f33e053c0e7b99b9a3121
Rahul Jain <72794264+Rahuljain06@users.noreply.github.com>
2024-05-28 20:36:58 +0530

    Time: 35 ms (55.06%), Space: 16.4 MB (88.80%) - LeetHub

--- 0551-student-attendance-record-i/0551-student-attendance-record-i.py
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.py
@@ -1,13 +1,15 @@
 class Solution:
     def checkRecord(self, s: str) -> bool:
-        if s.count("A")>=2:return False
         count=0
+        countA=1 if s[0]=="A" else 0
         for i in range(1,len(s)):
             if s[i]==s[i-1]=="L":
                 count+=2
             else:
                 count=0
-            if count>=3:
+            if s[i]=="A":
+                countA+=1
+            if count>=3 or countA>=2:
                 return False
         return True
             
