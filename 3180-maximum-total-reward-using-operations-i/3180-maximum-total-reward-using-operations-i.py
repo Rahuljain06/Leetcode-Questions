e5c49ea7c37f8a52e1f3add0bca8efab4b1950d3
Rahul Jain <72794264+Rahuljain06@users.noreply.github.com>
2024-06-09 09:53:06 +0530

    Time: 3221 ms (100.00%), Space: 781 MB (100.00%) - LeetHub

--- 3180-maximum-total-reward-using-operations-i/3180-maximum-total-reward-using-operations-i.py
+++ 3180-maximum-total-reward-using-operations-i/3180-maximum-total-reward-using-operations-i.py
@@ -1,21 +1,35 @@
 class Solution:
     def maxTotalReward(self, nums: List[int]) -> int:
-        nums = list(set(nums))
-        
+        # nums = list(set(nums))
+        # nums.sort()
+        # @cache
+        # def dp(i, cur):
+        #     if i >= len(nums):
+        #         return cur
+        #     skip = dp(i+1, cur)
+        #     if (nums[i] > cur):
+        #         operation = dp(i+1, cur+nums[i])
+        #     else:
+        #         operation = skip
+        #     return max(skip, operation)
+        # return dp(0, 0)
+    
+        nums=list(set(nums))
         nums.sort()
         @cache
-        def dp(i, cur):
-            
-            if i >= len(nums):
-                return cur
-        
-            skip = dp(i+1, cur)
-            if (nums[i] > cur):
-                operation = dp(i+1, cur+nums[i])
+        def dfs(i,curr):
+            if i>=len(nums):
+                return curr
+            nottake=dfs(i+1,curr)
+            if nums[i]>curr:
+                take=dfs(i+1,nums[i]+curr)
             else:
-                operation = skip
-            return max(skip, operation)
-        
-        return dp(0, 0)
+                take=0
+            
+            maxi=max(take,nottake)
+            return maxi
+        return dfs(0,0)
+
+
        
         
